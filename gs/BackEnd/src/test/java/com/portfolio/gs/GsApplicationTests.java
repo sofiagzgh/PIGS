c1918b561a07445148cc45f695fe7ffb91dc75d4
Sofia Gimenez <sofiagzgh@gmail.com>
2022-05-31 18:56:55 -0300

    Creacion carpeta BackEnd
    Creacion de proyecto en spring initializr

---
+++ gs/BackEnd/src/test/java/com/portfolio/gs/GsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.gs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
