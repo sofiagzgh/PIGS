dc5e290a0dec445acf325fb1b91f40c52e139603
Sofia Gimenez <sofiagzgh@gmail.com>
2022-07-08 17:37:34 -0300

    retoques

--- gs/FrontEnd/src/app/app.module.ts
+++ gs/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,11 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
+<<<<<<< Updated upstream
+=======
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -26,7 +31,8 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HysComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
